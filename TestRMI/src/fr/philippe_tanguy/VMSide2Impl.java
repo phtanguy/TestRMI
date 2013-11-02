eec1034a938174a0ccd6173a9f32f9cf59e69634
Philippe TANGUY <philippe.tanguy@telecom-bretagne.eu>
2013-11-02 09:48:00 +0100

    Un peu d'affichage pour vérifier que VMSide2Impl a bien été invoquée.

--- TestRMI/src/fr/philippe_tanguy/VMSide2Impl.java
+++ TestRMI/src/fr/philippe_tanguy/VMSide2Impl.java
@@ -8,6 +8,7 @@ public class VMSide2Impl implements IVMSide2
   @Override
   public int add(int i1, int i2) throws RemoteException
   {
+    System.out.println("VMSide2Impl --> add(" + i1 + ", " + i2 + ")");
     return (i1 + i2);
   }
   //-----------------------------------------------------------------------------
