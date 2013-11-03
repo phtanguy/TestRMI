a8698d68d395b77665d8c4996a13b270a084c899
Philippe TANGUY <philippe.tanguy@telecom-bretagne.eu>
2013-11-03 08:58:01 +0100

    Ménage dans les import.

--- TestRMI/src/fr/philippe_tanguy/ClientVMSideServer.java
+++ TestRMI/src/fr/philippe_tanguy/ClientVMSideServer.java
@@ -1,10 +1,7 @@
 package fr.philippe_tanguy;
 
 import java.net.MalformedURLException;
-import java.rmi.Naming;
 import java.rmi.NotBoundException;
-import java.rmi.RMISecurityManager;
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
